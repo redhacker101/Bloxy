67598722c3ca6c78fb8932a55980912d8c58d978
redhacker101 <125260154+redhacker101@users.noreply.github.com>
2026-05-16 19:33:34 +0800

    e
    e

--- ElectronApp/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("bloxy", {
-  launchGame: (gameId) => ipcRenderer.invoke("launch-game", gameId),
-
-  onStatus: (callback) => {
-    ipcRenderer.on("launcher-status", (event, message) => callback(message));
-  }
-});
